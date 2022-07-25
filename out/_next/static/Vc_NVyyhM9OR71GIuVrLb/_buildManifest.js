8e5f7f9eacd51e2eec0fac29a74151fa45930bbe
Ankit Mehta <ankitmehta94@gmail.com>
2022-07-24 20:28:53 -0400

    added out

---
+++ out/_next/static/Vc_NVyyhM9OR71GIuVrLb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-ab243316717ca7b2.js","static/chunks/47-08ddf7e35e95b135.js","static/chunks/pages/index-94d1eb7b3a0584c6.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
